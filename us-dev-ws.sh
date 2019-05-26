db9d68799479cc6f7df259044a1e9d6bf7eeeabe
Scott Ellis <wherami@gmail.com>
2019-05-26 16:05:53 -0400

    lotsa changes after first test

--- us-dev-ws.sh
+++ us-dev-ws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ~/P_HOME/CODE/US_DEV_WS/us-dev-ws.conf
+. /media/scotify/EXT_1/CODE/US_DEV_WS/us-dev-ws.conf
 
 # if [ ! -d $temp_dir ]; then
 #     mkdir $temp_dir;
@@ -9,11 +9,11 @@
 # cd $working_dir
 
 for ppa in $ppas; do
-  sudo apt-add-repository $ppa
+  sudo apt-add-repository -y $ppa
 done
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 for package in $packages; do
   sudo apt-get install -y $package
@@ -21,26 +21,26 @@ done
 
 if [[ $install_atom == "yes" ]]; then
   wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
-  sudo apt update
-  sudo apt install atom
+  sudo add-apt-repository -y "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+  sudo apt update -y
+  sudo apt install atom -y
 fi
 
 if [[ $create_gpg_key == "yes" ]]; then
   cat >"$HOME/gpg_conf" <<EOF
-     %echo Generating a basic OpenPGP key
-     Key-Type: default
-     Subkey-Type: default
-     Name-Real: $debfullname
-     Name-Comment: Created by us-dev-ws.sh
-     Name-Email: $debemail
-     %pubring "${debfullname// /_}".pub
-     %secring "${debfullname// /_}".sec
-     # Please don't change this to put plaintext-passwords here
-     %ask-passphrase
-     # Do a commit here, so that we can later print "done" :-)
-     %commit
-     %echo done
+%echo Generating a basic OpenPGP key
+Key-Type: default
+Subkey-Type: default
+Name-Real: $debfullname
+Name-Comment: Created by us-dev-ws.sh
+Name-Email: $debemail
+%pubring "${debfullname// /_}".pub
+%secring "${debfullname// /_}".sec
+# Please don't change this to put plaintext-passwords here
+%ask-passphrase
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
 EOF
 # Long format is used to prevent collisions on bigger systems - probably overkill
   gpg --send-keys --keyserver keyserver.ubuntu.com $(gpg --keyid-format long --list-keys $debemail)
@@ -62,28 +62,31 @@ fi
 
 echo "export DEBFULLNAME=\"$fullname\"" >> $bashrc
 echo "export DEBEMAIL=\"$email\"" >> $bashrc
-source $bashrc
+source "$bashrc"
 
 pbuilder-dist $ubuntu_release create
 
 if [[ $import_app_configs == "yes" ]]; then
 # Copy contents of source directory recursively, omitting rdiff's backup data of the archive
-  rsync -rva --progress --exclude "rdiff-backup-data" $app_configs_source. $HOME
+  rsync -rva --progress --exclude "rdiff-backup-data" "$app_configs_source/" "$HOME/"
 fi
 
 if [[ ! -z ${autostart_apps+x} ]]; then
+  if [ ! -d "$HOME/.config/autostart" ]; then
+       mkdir "$HOME/.config/autostart";
+  fi
   for app in $autostart_apps; do
-    cat >"$HOME/.config/autostart/$app.desk" <<EOF
-      [Desktop Entry]
-      Encoding=UTF-8
-      Type=Application
-      Name=$app
-      Comment=Created by ws-dev-ws.sh
-      Exec=$app
-      OnlyShowIn=XFCE;
-      StartupNotify=false
-      Terminal=false
-      Hidden=false
+    cat >"$HOME/.config/autostart/$app.desktop" <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=$app
+Comment=Created by ws-dev-ws.sh
+Exec=$app
+OnlyShowIn=XFCE;
+StartupNotify=false
+Terminal=false
+Hidden=false
 EOF
   done
 fi
