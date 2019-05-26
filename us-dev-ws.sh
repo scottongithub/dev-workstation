6ed910f9980c3e0fdd4219f65e95d64baf09f5bd
Scott <wherami@gmail.com>
2019-05-25 23:12:12 -0400

    added packages added auto-start packages

--- us-dev-ws.sh
+++ us-dev-ws.sh
@@ -9,24 +9,24 @@
 # cd $working_dir
 
 for ppa in $ppas; do
-  sudo apt-add-repository $ppa | tee -a $logfile
+  sudo apt-add-repository $ppa
 done
-sudo apt update | tee -a $logfile
-sudo apt upgrade | tee -a $logfile
 
+sudo apt update
+sudo apt upgrade
 
 for package in $packages; do
-  sudo apt-get install -y $package | tee -a $logfile
+  sudo apt-get install -y $package
 done
 
-if install_atom=yes; then
+if [[ $install_atom == "yes" ]]; then
   wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
   sudo apt update
   sudo apt install atom
 fi
 
-if create_gpg_key=yes; then
+if [[ $create_gpg_key == "yes" ]]; then
   cat >"$HOME/gpg_conf" <<EOF
      %echo Generating a basic OpenPGP key
      Key-Type: default
@@ -42,39 +42,50 @@ if create_gpg_key=yes; then
      %commit
      %echo done
 EOF
-# Long format is used to prevent collisions on bigger systems
+# Long format is used to prevent collisions on bigger systems - probably overkill
   gpg --send-keys --keyserver keyserver.ubuntu.com $(gpg --keyid-format long --list-keys $debemail)
 fi
 
-if import_gpg_keys=yes; then
-  cp -r $gpg_key_source "$HOME/.gnupg"
+if [[ $import_gpg_keys == "yes" ]]; then
+# Copy recursively, interactively (prompt before replacing)
+  cp -ri $gpg_key_source "$HOME"
 fi
 
-if create_ssh_key=yes; then
-  ssh-keygen -t rsa | tee -a $logfile
+if [[ $create_ssh_key == "yes" ]]; then
+  ssh-keygen -t rsa -C "$fullname on $(hostname)"
 fi
 
-if import_ssh_keys=yes; then
-  cp -r $ssh_key_source "$HOME/.ssh"
+if [[ $import_ssh_keys == "yes" ]]; then
+# Copy recursively, interactively (prompt before replacing)
+  cp -ri $ssh_key_source "$HOME"
 fi
+
+echo "export DEBFULLNAME=\"$fullname\"" >> $bashrc
+echo "export DEBEMAIL=\"$email\"" >> $bashrc
+source $bashrc
 
 pbuilder-dist $ubuntu_release create
 
+if [[ $import_app_configs == "yes" ]]; then
+# Copy contents of source directory recursively, omitting rdiff's backup data of the archive
+  rsync -rva --progress --exclude "rdiff-backup-data" $app_configs_source. $HOME
+fi
 
-echo "export DEBFULLNAME=\"$debfullname\"" >> $bashrc
-echo "export DEBEMAIL=\"$debemail\"" >> $bashrc
+if [[ ! -z ${autostart_apps+x} ]]; then
+  for app in $autostart_apps; do
+    cat >"$HOME/.config/autostart/$app.desk" <<EOF
+      [Desktop Entry]
+      Encoding=UTF-8
+      Type=Application
+      Name=$app
+      Comment=Created by ws-dev-ws.sh
+      Exec=$app
+      OnlyShowIn=XFCE;
+      StartupNotify=false
+      Terminal=false
+      Hidden=false
+EOF
+  done
+fi
 
-
-for backup_dir in $BackupDirs; do
-
-Destination="${backup_dir}/${BackupName}_$(hostname)"
-
-  if [ ! -d $Destination ]; then
-      mkdir $Destination;
-  fi
-
-  rdiff-backup -v9 $TempDir $Destination
-
-done
-
-rm -rf $TempDir
+#rm -rf $TempDir
